cab9f05b700877fbbab7a503d022a048366c8054
SUGAMA Tatsuo <gama@sstinc.co.jp>
2017-09-09 18:51:41 +0900

    アニメーションさせる

--- main.go
+++ main.go
@@ -18,18 +18,42 @@ func main() {
     // 最後にwindowの後始末
     defer window.Destroy()
 
-    // windowに描画するためのsurfaceオブジェクトを取得
-    surface, err := window.GetSurface()
-    if err != nil {
+    // windowに描画するためのrendererオブジェクトを取得
+    render, renderErr := sdl.CreateRenderer(window, -1, 0)
+    if renderErr != nil {
         // あまり失敗しないと思われるので手抜きのエラー処理
         panic(err)
     }
+    // 最後にrendererの後始末
+    defer render.Destroy()
 
-    // 試しに四角を描いてみる
-    rect := sdl.Rect{X: 0, Y: 0, W:200, H:200}
-    surface.FillRect(&rect, 0xffff0000)
-    // windowのsurfinceを更新することで画面を更新する
-    window.UpdateSurface()
+    // 指定の色で全体をクリア
+    render.SetDrawColor(0, 0, 0, 255)
+    render.Clear()
+    // renderをwindowに反映
+    render.Present()
+
+    for i := 0; i < 10000; i++ {
+
+        // 毎回の画面の更新
+
+        // 指定の色で全体をクリア
+        render.SetDrawColor(0, 0, 0, 255)
+        render.Clear()
+
+        // 指定の色、場所に四角を描く
+        // だんだんと動くように、カウンタを元に座標計算
+        render.SetDrawColor(255, 0, 0, 255)
+        rect := sdl.Rect{X:0, Y:0, W:int32(i % 800), H:200}
+        render.FillRect(&rect)
+
+        // renderをwindowに反映
+        render.Present()
+
+        // ちょっとだけ待つ
+        sdl.Delay(10 * 1)
+
+    }
 
     // しばし表示している
     sdl.Delay(1000 * 2)
